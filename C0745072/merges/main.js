712fc90a03ba8d764d46d133f204c869edc147e7
CHERIN LINSON <c4cheriachan@gmail.com>
2019-02-04 17:00:15 -0500

    5 'o' clock submit

--- C0745072/merges/main.js
+++ C0745072/merges/main.js
@@ -1,2 +1,5 @@
-﻿var date = new Date();
-document.body.innerHTML = "<H1> Today is " + date + " </h1>";
+﻿function f1()
+{
+    var date = new Date();
+    document.body.innerHTML = "<H1> Today is " + date + " </h1>";
+}
